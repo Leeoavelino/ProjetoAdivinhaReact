51ec183608fb029de6793cd78b0d3e7018c15693
Leeoavelino <leoave46@gmail.com>
2023-11-16 11:35:34 -0300

    ajuste final

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,20 @@
+import './GameOver.css'
+
+export default function GameOver({retry, score}){
+    return(
+        <div>
+            <h1>
+               Fim de jogo!
+            </h1>
+
+            <h2>
+                A sua pontuação foi: <span> {score} </span>
+            </h2>
+
+            <button onClick={retry}>
+                Reiniciar
+            </button>
+
+        </div>
+    )
+}
